f2f2b5f7ca0ad0afe633ac325a74b0f241aa1f3d
Quinto-A <acquinoquinto5@gmail.com>
2025-03-17 23:20:43 +0300

    routes.py

---
+++ StreakyReads/models/book.py
@@ -0,0 +1,8 @@
+from .. import db
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title =  db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    total_pages = db.Column(db.Integer, nullable=False)
+    pages_read = db.Column(db.Integer, default=0)
---
+++ StreakyReads/routes/book_routes.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request, jsonify
+from StreakyReads.models.book import Book
+from StreakyReads import db 
+
+main = Blueprint('main', __name__)
+
+@main.route('/books', methods=['POST'])
+def add_book():
+    data = request.json
+    new_book = Book(title=data['title'], author=data['author'], total_pages=data['total-pages'])
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify({'message': 'Book added successfully'}), 201
+
+@main.route('/books', methods=['GET'])
+def get_books():
+    books = Book.query.all()
+    return jsonify([{'id': book.id, 'title': book.title, 'author': book.author, 'total_pages': book.total_pages, 'pages_read': book.pages_read} for book in books])
+
+@main.route('/books/<int:book_id>/progress', methods=[PATCH])
+def update_progress(book_id):
+    data = request.json
+    book = Book.query.get_or_404(book_id)
+    book.pages_read = min(book.pages_read + data['pages_read'], book.total_pages)
+    db.session.commit()
+    return jsonify({'message': 'Progress updated successfully'})
