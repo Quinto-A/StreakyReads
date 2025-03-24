a2eefd423cd0f5d2a96e1797ac71caf800d8ba76
Quinto-A <acquinoquinto5@gmail.com>
2025-03-24 22:48:57 +0300

    landing page

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,14 +1,9 @@
 from flask import Blueprint, request, jsonify
-from app.models.book import Book
-from app import db
+from app.models.book import Book, db
 
-main = Blueprint('main', __name__, url_prefix='/api')
+book_routes = Blueprint('book_routes', __name__)
 
-@main.route('/')
-def home():
-    return jsonify({'message': 'Welcome to SteakyReads API!'})
-
-@main.route('/books', methods=['POST'])
+@book_routes.route('/books', methods=['POST'])
 def add_book():
     data = request.json
     new_book = Book(title=data['title'], author=data['author'], total_pages=data['total-pages'])
@@ -16,12 +11,12 @@ def add_book():
     db.session.commit()
     return jsonify({'message': 'Book added successfully'}), 201
 
-@main.route('/books', methods=['GET'])
+@book_routes.route('/books', methods=['GET'])
 def get_books():
     books = Book.query.all()
     return jsonify([{'id': book.id, 'title': book.title, 'author': book.author, 'total_pages': book.total_pages, 'pages_read': book.pages_read} for book in books])
 
-@main.route('/books/<int:book_id>/progress', methods=['PATCH'])
+@book_routes.route('/books/<int:book_id>/progress', methods=['PATCH'])
 def update_progress(book_id):
     data = request.json
     book = Book.query.get_or_404(book_id)
--- run.py
+++ run.py
@@ -1,6 +1,20 @@
-from app import create_app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = create_app()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+db = SQLAlchemy(app)
+
+from app.routes.book_routes import book_routes
+from app.routes.main import main
+
+app.register_blueprint(book_routes, url_prefix='/api')
+app.register_blueprint(main)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
